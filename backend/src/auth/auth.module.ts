16defc9448cd6e96bada415a41e4d397b28d52d3
Dremak <88313517+Dremak@users.noreply.github.com>
2023-11-01 22:15:53 +0100

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Uauth } from './entities/uauth.entity';
 import { AuthController } from './auth.controller';
-import { Oauth2Strategy } from './oauth2.service';
+import { AuthService } from './oauth2.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Uauth])],
-  controllers: [AuthController],
-  providers: [Oauth2Strategy],
+	imports: [HttpModule, ConfigModule, TypeOrmModule.forFeature([Uauth])],
+	controllers: [AuthController],
+	providers: [AuthService, ConfigService],
 })
 export class AuthModule {}
