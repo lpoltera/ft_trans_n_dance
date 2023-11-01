22ae3fa828dd3b40e326c0b0ff073f455a4eef2f
lpoltera <lpoltera@student.42lausanne.ch>
2023-11-01 10:39:31 +0100

    config tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,20 @@
+// import type { Config } from "tailwindcss";
+
+// export default {
+//   content: ["./src/**/*.{html,ts,tsx}"],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "home-bkg": "url('/src/assets')",
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/forms")],
+// } satisfies Config;
+
+
 import type { Config } from "tailwindcss";
+import formsPlugin from "@tailwindcss/forms";
 
 export default {
   content: ["./src/**/*.{html,ts,tsx}"],
@@ -9,5 +25,5 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [formsPlugin],
 } satisfies Config;
