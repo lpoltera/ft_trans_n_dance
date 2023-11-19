e04e4e1b61bf99daa454cacbfb4867de6182772c
lpoltera <lpoltera@student.42lausanne.ch>
2023-11-19 16:35:54 +0100

    block Icon button in profilePage and chatPage -> Active

--- frontend/src/Components/Chat/FriendChatListItem.tsx
+++ frontend/src/Components/Chat/FriendChatListItem.tsx
@@ -1,52 +1,65 @@
 import {
 	NoSymbolIcon,
 	UserIcon,
-  } from "@heroicons/react/24/outline";
+} from "@heroicons/react/24/outline";
 import './Chat.css';
+import axios from "axios";
 
 
 
-  interface User {
+interface User {
 	id?: number;
 	username: string;
 	status: string;
 	avatar: string;
-  }
+}
 
-  interface Props {
+interface Props {
 	ami: User;
 	handleChatButtonClick: (ami: User) => void;
-  }
+}
 
-  const FriendChatListItem = ({ ami, handleChatButtonClick }: Props) => {
+const FriendChatListItem = ({ ami, handleChatButtonClick }: Props) => {
+
+
+	const changeFriendshipStatus = async () => {
+		const isConfirmed = window.confirm('Êtes-vous sûr de vouloir bloquer cet utilisateur?');
+
+		if (isConfirmed) {
+			await axios.patch("api/friends/" + ami.username, { status: "blocked" });
+			window.location.reload();
+		}
+	};
 
 	return (
-	  <>
-		<button className="user-list__item hover:bg-neutral-800 rounded-md" onClick={() => handleChatButtonClick(ami)}>
-			<div className="user-list__item__avatar">
-			  <img
-				src={ami.avatar}
-				alt="user'avatar profile"
-				className="w-12 h-12 rounded-full object-cover"
-			  />
-			  <div className="user-list__item__infos">
-				<div className="text-xl">{ami.username}</div>
-				<div className="text-sm text-gray-400">{ami.status}</div>
-				<button
-					type="button"
-					className="w-6 h-6 opacity-50 hover:opacity-100">
-					<NoSymbolIcon />
-				</button>
-				<button
-					type="button"
-					className="w-6 h-6 opacity-50 hover:opacity-100">
-					<UserIcon />
-				</button>
-			  </div>
-			</div>
-		</button>
-	  </>
+		<>
+			<button className="user-list__item hover:bg-neutral-800 rounded-md" onClick={() => handleChatButtonClick(ami)}>
+				<div className="user-list__item__avatar">
+					<img
+						src={ami.avatar}
+						alt="user'avatar profile"
+						className="w-12 h-12 rounded-full object-cover"
+					/>
+					<div className="user-list__item__infos">
+						<div className="text-xl">{ami.username}</div>
+						<div className="text-sm text-gray-400">{ami.status}</div>
+						<button
+							type="button"
+							className="w-6 h-6 opacity-50 hover:opacity-100"
+							onClick={() => changeFriendshipStatus()}
+						>
+							<NoSymbolIcon />
+						</button>
+						<button
+							type="button"
+							className="w-6 h-6 opacity-50 hover:opacity-100">
+							<UserIcon />
+						</button>
+					</div>
+				</div>
+			</button>
+		</>
 	);
-  };
+};
 
-  export default FriendChatListItem;
+export default FriendChatListItem;
--- frontend/src/Components/FriendProfilRow.tsx
+++ frontend/src/Components/FriendProfilRow.tsx
@@ -1,66 +1,78 @@
 import {
-  ChatBubbleBottomCenterTextIcon,
-  NoSymbolIcon,
-  UserIcon,
+	ChatBubbleBottomCenterTextIcon,
+	NoSymbolIcon,
+	UserIcon,
 } from "@heroicons/react/24/outline";
+import axios from "axios";
 
 interface Ami {
-  id?: number;
-  username: string;
-  status: string;
-  avatar: string;
+	id?: number;
+	username: string;
+	status: string;
+	avatar: string;
 }
 
 interface Props {
-  ami: Ami;
+	ami: Ami;
 }
 
 const navigateToChat = () => {
-  window.location.href = "/chat";
+	window.location.href = "/chat";
 };
 
 const FriendProfilRow = ({ ami }: Props) => {
-  return (
-    <>
-      <div className="flex justify-between items-center pr-4 hover:bg-neutral-800 rounded-md">
-        <a href={"/user?id=" + ami.id} className="py-4 px-4">
-          <div className="flex justify-center gap-4">
-            <img
-              src={ami.avatar}
-              alt=""
-              className="w-12 h-12 rounded-full object-cover"
-            />
-            <div className="flex flex-col justify-start">
-              <div className="text-xl">{ami.username}</div>
-              <div className="text-sm text-gray-400">{ami.status}</div>
-            </div>
-          </div>
-          <div className="text-xl flex justify-end items-center"></div>
-        </a>
-        <div className="grid grid-flow-col grid-cols-3 gap-2">
-          <button
-            type="button"
-            className="w-6 h-6 opacity-50 hover:opacity-100"
-          >
-            <NoSymbolIcon />
-          </button>
-          <button
-            type="button"
-            className="w-6 h-6 opacity-50 hover:opacity-100"
-            onClick={navigateToChat}
-          >
-            <ChatBubbleBottomCenterTextIcon />
-          </button>
-          <button
-            type="button"
-            className="w-6 h-6 opacity-50 hover:opacity-100"
-          >
-            <UserIcon />
-          </button>
-        </div>
-      </div>
-    </>
-  );
+
+	const changeFriendshipStatus = async () => {
+		const isConfirmed = window.confirm('Êtes-vous sûr de vouloir bloquer cet utilisateur?');
+
+		if (isConfirmed) {
+			await axios.patch("api/friends/" + ami.username, { status: "blocked" });
+			window.location.reload();
+		}
+	};
+
+	return (
+		<>
+			<div className="flex justify-between items-center pr-4 hover:bg-neutral-800 rounded-md">
+				<a href={"/user?id=" + ami.id} className="py-4 px-4">
+					<div className="flex justify-center gap-4">
+						<img
+							src={ami.avatar}
+							alt=""
+							className="w-12 h-12 rounded-full object-cover"
+						/>
+						<div className="flex flex-col justify-start">
+							<div className="text-xl">{ami.username}</div>
+							<div className="text-sm text-gray-400">{ami.status}</div>
+						</div>
+					</div>
+					<div className="text-xl flex justify-end items-center"></div>
+				</a>
+				<div className="grid grid-flow-col grid-cols-3 gap-2">
+					<button
+						type="button"
+						className="w-6 h-6 opacity-50 hover:opacity-100"
+						onClick={() => changeFriendshipStatus()}
+					>
+						<NoSymbolIcon />
+					</button>
+					<button
+						type="button"
+						className="w-6 h-6 opacity-50 hover:opacity-100"
+						onClick={navigateToChat}
+					>
+						<ChatBubbleBottomCenterTextIcon />
+					</button>
+					<button
+						type="button"
+						className="w-6 h-6 opacity-50 hover:opacity-100"
+					>
+						<UserIcon />
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default FriendProfilRow;
