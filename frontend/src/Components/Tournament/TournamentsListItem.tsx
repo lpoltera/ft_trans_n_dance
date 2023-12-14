4947ea3defbab02f4d350d534ae82305b04476b8
lpoltera <lpoltera@student.42lausanne.ch>
2023-12-14 16:06:52 +0100

    TournamentsListItem displays TournamentName & TournamentCreator

--- frontend/src/Components/Tournament/TournamentsListItem.tsx
+++ frontend/src/Components/Tournament/TournamentsListItem.tsx
@@ -5,15 +5,24 @@ import { PencilSquareIcon, EyeIcon, TrashIcon } from "@heroicons/react/24/outlin
 
 interface TournamentsListItemProps {
 	handleStatsButtonClick: (tournamentName: string) => void;
-	name: string;
-	creator: string;
+	// name: string;
+	// creator: string;
+	tournoi: string[];
 }
 
+// interface Tournament {
+// 	name: string;
+// 	creator: string;
+// }
 
-const TournamentsListItem: React.FC<TournamentsListItemProps> = ({ name, creator, handleStatsButtonClick }) => {
 
-	console.log("name", name)
-	console.log("creator", creator)
+const TournamentsListItem: React.FC<TournamentsListItemProps> = ({ tournoi, handleStatsButtonClick }) => {
+
+	// console.log("name", name)
+	console.log("tournoi", tournoi)
+	console.log("tournoi", tournoi[0])
+	// console.log("creator", tournoi.creator)
+	// console.log("name", tournoi.name)
 
 	return (
 		<>
@@ -21,8 +30,8 @@ const TournamentsListItem: React.FC<TournamentsListItemProps> = ({ name, creator
 				<a href="#" className="py-4 px-4">
 					<div className="flex justify-center gap-4">
 						<div className="flex flex-col justify-start">
-							<p className="text-xl">{name} </p>
-							<p className="text-sm">Créé par {creator}</p>
+							<p className="text-xl">{tournoi[0]} </p>
+							<p className="text-sm">Créé par {tournoi[1]}</p>
 						</div>
 					</div>
 				</a>
@@ -37,7 +46,7 @@ const TournamentsListItem: React.FC<TournamentsListItemProps> = ({ name, creator
 
 					{/* [Modal toggle] Watch brackets */}
 					<button type="button" className="w-6 h-6 opacity-50 hover:opacity-100"
-						onClick={() => handleStatsButtonClick(name)}
+						onClick={() => handleStatsButtonClick(tournoi[0])}
 					>
 						<EyeIcon />
 					</button>
