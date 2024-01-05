5b3bb6c3024724854a015bf7b5e24f0e2da2a260
bgrafe-d <bgrafe-d@student.42lausanne.ch>
2024-01-05 21:56:54 +0100

    Animation ball landing page => en cours + fix probleme date chat page

--- frontend/src/Components/Chat/Chat.tsx
+++ frontend/src/Components/Chat/Chat.tsx
@@ -8,132 +8,136 @@ import RightMessageContainer from "./RightMessageContainer";
 import { ChatMessage } from "../../models/Notifications";
 
 interface Props {
-	ami: User;
+  ami: User;
 }
 
 const Chat = ({ ami }: Props) => {
-	const [messages, setMessages] = useState<ChatMessage[]>([]);
-	const [newMessage, setNewMessage] = useState("");
-	const { socket, socketLoading } = useNotificationContext();
-	const messagesEndRef = useRef<HTMLDivElement | null>(null);
-	const [currentUser, setCurrentUser] = useState<User | null>(null);
-	// const { user, loadingUser } = useUserContext();
-	const scrollToBottom = () => {
-		if (messagesEndRef.current) {
-			messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-		}
-	};
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  const [newMessage, setNewMessage] = useState("");
+  const { socket, socketLoading } = useNotificationContext();
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  // const { user, loadingUser } = useUserContext();
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-	useEffect(scrollToBottom, [messages]);
+  useEffect(scrollToBottom, [messages]);
 
-	useEffect(() => {
+  useEffect(() => {
+    async function fetchCurrent() {
+      try {
+        const response = await axios.get<User>("/api/my-name");
+        setCurrentUser(response.data);
+        console.log("my name :", response.data.username);
+        return response.data.username;
+      } catch (err) {
+        console.error(err);
+        return null;
+      }
+    }
 
-		async function fetchCurrent() {
-			try {
-				const response = await axios.get<User>("/api/my-name");
-				setCurrentUser(response.data);
-				console.log("my name :", response.data.username);
-				return response.data.username;
-			} catch (err) {
-				console.error(err);
-				return null;
-			}
-		}
+    // if (loadingUser) return;
+    // if (socketLoading) return;
+    // console.log("socket :", socket);
 
-		// if (loadingUser) return;
-		// if (socketLoading) return;
-		// console.log("socket :", socket);
+    async function fetchMessages() {
+      const name = await fetchCurrent();
+      if (name) {
+        try {
+          const response = await axios.get(
+            "/api/chat/all/" + name + "/" + ami.username
+          );
+          console.log("response.data :", response.data);
+          setMessages(response.data);
+        } catch (error) {
+          console.error("Error fetching messages:", error);
+        }
+      }
+    }
+    fetchMessages();
 
-		async function fetchMessages() {
-			const name = await fetchCurrent();
-			if (name) {
-				try {
-					const response = await axios.get(
-						"/api/chat/all/" + name + "/" + ami.username
-					);
-					console.log("response.data :", response.data);
-					setMessages(response.data);
-				} catch (error) {
-					console.error("Error fetching messages:", error);
-				}
-			}
-		}
-		fetchMessages();
+    return () => {
+      if (socket) socket.off("recMessage");
+    };
+  }, [ami]);
 
-		return () => {
-			if (socket) socket.off("recMessage");
-		};
-	}, [ami]);
+  useEffect(() => {
+    if (socket && currentUser?.username && ami.username) {
+      if (!socket.hasListeners("recMessage")) {
+        socket.on("recMessage", (message) => {
+          if (
+            (message.sender === ami.username &&
+              message.receiver === currentUser.username) ||
+            (message.sender === currentUser.username &&
+              message.receiver === ami.username)
+          ) {
+            if (message.sender !== currentUser.username) {
+              setMessages((prevMessages) => [...prevMessages, message]);
+            }
+          }
+        });
+      }
+    }
 
-	useEffect(() => {
-		if (socket && currentUser?.username && ami.username) {
-			if (!socket.hasListeners("recMessage")) {
-				socket.on("recMessage", (message) => {
-					if ((message.sender === ami.username && message.receiver === currentUser.username) ||
-						(message.sender === currentUser.username && message.receiver === ami.username)) {
-						if (message.sender !== currentUser.username) {
+    return () => {
+      if (socket) {
+        socket.off("recMessage");
+      }
+    };
+  }, [socket, currentUser?.username, ami.username]);
 
-							setMessages((prevMessages) => [...prevMessages, message]);
-						}
-					}
-				});
-			}
-		}
+  // Update messages state when a message is sent
+  const sendMessage = (e: FormEvent, messageContent: string) => {
+    e.preventDefault();
+    const currentTime = new Date();
+    if (currentUser?.username) {
+      const message = {
+        sender: currentUser.username,
+        receiver: ami.username,
+        text: messageContent,
+        createdAt: currentTime.toISOString(),
+      };
 
-		return () => {
-			if (socket) {
-				socket.off("recMessage");
-			}
-		};
-	}, [socket, currentUser?.username, ami.username]);
+      socket?.emit("sendMessage", message);
 
-	// Update messages state when a message is sent
-	const sendMessage = (e: FormEvent, messageContent: string) => {
-		e.preventDefault();
-		if (currentUser?.username) {
-			const message = {
-				sender: currentUser.username,
-				receiver: ami.username,
-				text: messageContent,
-			};
+      setMessages((prevMessages) => [...prevMessages, message]);
+      setNewMessage("");
+    }
+  };
 
-			socket?.emit("sendMessage", message);
-
-			setMessages((prevMessages) => [...prevMessages, message]);
-			setNewMessage("");
-		}
-	};
-
-	return (
-		<>
-			<div className="grow overflow-auto mb-2 flex">
-				<ul className="px-2 mt-auto w-full">
-					{messages.map((message, index) => (
-						<li key={index}>
-							{message.sender === currentUser?.username ? (
-								<RightMessageContainer user={currentUser} message={message} />
-							) : (
-								<LeftMessageContainer user={ami} message={message} />
-							)}
-						</li>
-					))}
-					<div ref={messagesEndRef} />
-				</ul>
-			</div>
-			<div className="shrink">
-				<form onSubmit={(e) => sendMessage(e, newMessage)}>
-					<input
-						type="text"
-						className="border-white rounded-md px-3 py-2 bg-transparent w-full focus:border-cyan-500 focus:outline-none appearance-none"
-						value={newMessage}
-						onChange={(e) => setNewMessage(e.target.value)}
-						placeholder="Ecrivez un message..."
-					/>
-					<input type="submit" className="hidden" />
-				</form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="grow overflow-auto mb-2 flex">
+        <ul className="px-2 mt-auto w-full">
+          {messages.map((message, index) => (
+            <li key={index}>
+              {message.sender === currentUser?.username ? (
+                <RightMessageContainer user={currentUser} message={message} />
+              ) : (
+                <LeftMessageContainer user={ami} message={message} />
+              )}
+            </li>
+          ))}
+          <div ref={messagesEndRef} />
+        </ul>
+      </div>
+      <div className="shrink">
+        <form onSubmit={(e) => sendMessage(e, newMessage)}>
+          <input
+            type="text"
+            className="border-white rounded-md px-3 py-2 bg-transparent w-full focus:border-cyan-500 focus:outline-none appearance-none"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            placeholder="Ecrivez un message..."
+          />
+          <input type="submit" className="hidden" />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Chat;
--- frontend/src/Components/FormLogin.tsx
+++ frontend/src/Components/FormLogin.tsx
@@ -3,91 +3,90 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const FormLogin = () => {
-	const navigate = useNavigate();
-	const [login, setLogin] = useState({
-		username: "",
-		password: "",
-	});
-	const loginUser = async (e: FormEvent) => {
-		e?.preventDefault();
-		await axios
-			.post("/api/login", login, {
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-			})
-			.then((response) => {
-				const twoFaEnable = response.data.usertwoFA;
-				console.log("session.twoFaEnable = ", response.data.usertwoFA);
-				if (twoFaEnable === true) navigate("/twofa-verify");
-				else {
-					navigate("/accueil");
-				}
-			})
-			.catch((err) => {
-				setLogin({
-					username: login.username,
-					password: "",
-				});
-				if (err.response && err.response.data && err.response.data.message) {
-					const errorMessage = err.response.data.message;
-					return alert("Error: " + errorMessage);
-				} else {
-					return alert("Error: " + err.message);
-				}
-			});
-	};
-	const onChangeForm = (e: ChangeEvent<HTMLInputElement>) => {
-		if (e.target.name === "username") {
-			setLogin({ ...login, username: e.target.value });
-		} else if (e.target.name === "password") {
-			setLogin({ ...login, password: e.target.value });
-		}
-	};
-	return (
-		<>
-			<form
-				className="min-w-fit w-96 flex flex-col"
-				name="loginForm"
-				onSubmit={(e) => loginUser(e)}
-			>
-				<div>
-					<h1 className="text-xl">CONNEXION</h1>
-				</div>
-				<div className="flex flex-col gap-2 border border-x-0 border-t-0 border-gray-400 py-4">
-					<input
-						type="text"
-						id="username"
-						value={login.username}
-						name="username"
-						className="border border-white px-3 py-2 bg-transparent text-white rounded-md"
-						placeholder="Pseudo"
-						onChange={(e) => onChangeForm(e)}
-					/>
-					<input
-						type="password"
-						value={login.password}
-						name="password"
-						id="password"
-						className="border border-white px-3 py-2 bg-transparent text-white rounded-md"
-						placeholder="Mot de passe"
-						onChange={(e) => onChangeForm(e)}
-					/>
-				</div>
-				<div className="flex flex-row mt-4 justify-between items-center">
-					<button
-						type="submit"
-						className="bg-white text-black py-2 px-4 rounded-md border border-white"
-					>
-						Se connecter
-					</button>
-					<a href="/forgot-password" className="text-gray-400 hover:text-white">
-						mot de passe oublié ?
-					</a>
-				</div>
-			</form>
-		</>
-	);
+  const navigate = useNavigate();
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  });
+  const loginUser = async (e: FormEvent) => {
+    e?.preventDefault();
+    await axios
+      .post("/api/login", login, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then((response) => {
+        const twoFaEnable = response.data.usertwoFA;
+        console.log("session.twoFaEnable = ", response.data.usertwoFA);
+        if (twoFaEnable === true) navigate("/twofa-verify");
+        else {
+          navigate("/accueil");
+        }
+      })
+      .catch((err) => {
+        setLogin({
+          username: login.username,
+          password: "",
+        });
+        if (err.response && err.response.data && err.response.data.message) {
+          const errorMessage = err.response.data.message;
+          return alert("Error: " + errorMessage);
+        } else {
+          return alert("Error: " + err.message);
+        }
+      });
+  };
+  const onChangeForm = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "username") {
+      setLogin({ ...login, username: e.target.value });
+    } else if (e.target.name === "password") {
+      setLogin({ ...login, password: e.target.value });
+    }
+  };
+  return (
+    <>
+      <form
+        className="min-w-fit w-96 flex flex-col border border-white rounded-md p-20 gap-4"
+        name="loginForm"
+        onSubmit={(e) => loginUser(e)}
+      >
+        <div>
+          <h1 className="text-xl flex flex-col items-center pb-15">
+            CONNEXION
+          </h1>
+        </div>
+        <div className="flex flex-col gap-2 border border-x-0 border-t-0 border-gray-400 py-10">
+          <input
+            type="text"
+            id="username"
+            value={login.username}
+            name="username"
+            className="border border-[#f67539] px-3 py-2 bg-transparent text-white rounded-md"
+            placeholder="Pseudo"
+            onChange={(e) => onChangeForm(e)}
+          />
+          <input
+            type="password"
+            value={login.password}
+            name="password"
+            id="password"
+            className="border border-[#f67539] px-3 py-2 bg-transparent text-white rounded-md"
+            placeholder="Mot de passe"
+            onChange={(e) => onChangeForm(e)}
+          />
+        </div>
+        <div className="flex flex-col mt-5 justify-between items-center">
+          <button
+            type="submit"
+            className="py-2 px-4 bg-cyan-700  text-white rounded-md hover:bg-[#f67539]"
+          >
+            Se connecter
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default FormLogin;
